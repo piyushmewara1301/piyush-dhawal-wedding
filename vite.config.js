fbbfc0eb9a61fe47f659c244b8d45d53b5fdb3f5
piyush mewara <piyushmewara@piyushs-Air.lan>
2025-11-24 00:19:15 +0530

    updating the code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/piyush-dawal-wedding/",
   plugins: [react()],
+  base: "/piyush-dhawal-wedding/", 
 });
